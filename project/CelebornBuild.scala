1ab0c9687124a648f0b4546221530328a0f19291
senthh <senthil.kumar@acceldata.io>
2026-09-11 09:33:40 +0530

    OSV-41138 - CVE - Upgrade Jackson to 2.18.8
    Fixes 1 CVE in Jackson (2.18.6 → 2.18.8)

--- project/CelebornBuild.scala
+++ project/CelebornBuild.scala
@@ -66,7 +66,7 @@ object Dependencies {
   val ratisVersion = "3.1.3"
   val roaringBitmapVersion = "1.0.6"
   val rocksdbJniVersion = "9.10.0"
-  val jacksonVersion = "2.15.3"
+  val jacksonVersion = "2.18.8"
   val jakartaActivationApiVersion = "1.2.1"
   val scalatestMockitoVersion = "1.17.14"
   val scalatestVersion = "3.2.16"
